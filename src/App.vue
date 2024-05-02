35341b63a4870ee20e0103768f79cda66e71ccce
tony11tony11t <tony11tony11t@gmail.com>
2024-05-02 19:20:33 +0800

    feat: add resize function to handle RWD

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { onMounted, onUnmounted } from 'vue'
+
+//TODO: RWD scale function
+const handleResize = () => {
+  var siteWidth = 390
+  var scale = screen.width / siteWidth
+
+  const meta = document?.querySelector('meta[name="viewport"]')
+
+  meta?.setAttribute('content', `width=${siteWidth}, initial-scale=${scale}`)
+}
+
+onMounted(() => {
+  handleResize()
+  window.addEventListener('resize', handleResize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize)
+})
 </script>
 
 <template>
